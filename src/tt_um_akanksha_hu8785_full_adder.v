16a0747b59eade6e285318ffc61dfaa9b34ca850
hu8785 <patilaku96@gmail.com>
2026-04-24 21:25:27 -0400

    Update tt_um_akanksha_hu8785_full_adder.v

--- src/tt_um_akanksha_hu8785_full_adder.v
+++ src/tt_um_akanksha_hu8785_full_adder.v
@@ -1,25 +1,26 @@
 `default_nettype none
 
 module tt_um_akanksha_hu8785_full_adder (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
     wire A   = ui_in[0];
     wire B   = ui_in[1];
     wire Cin = ui_in[2];
 
-    wire Sum, Cout;
+    wire Sum;
+    wire Cout;
+
     assign Sum  = A ^ B ^ Cin;
     assign Cout = (A & B) | (Cin & (A ^ B));
 
-    // Assign outputs
     assign uo_out[0] = Sum;
     assign uo_out[1] = Cout;
     assign uo_out[2] = 1'b0;
@@ -29,11 +30,11 @@ module tt_um_akanksha_hu8785_full_adder (
     assign uo_out[6] = 1'b0;
     assign uo_out[7] = 1'b0;
 
-    // All output pins must be assigned
-    assign uio_out = 8'b0;
-    assign uio_oe  = 8'b0;
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
 
-    // List all unused inputs to prevent warnings
-    wire _unused = ena ^ clk ^ rst_n ^ |uio_in ^ |ui_in[7:3];
+    wire _unused = &{ena, clk, rst_n, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
+
+`default_nettype wire
